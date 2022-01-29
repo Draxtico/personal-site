aae00e6dbca746b678a29af32c157ece64b6aadb
Roberto Mirabella <bronzetto@outlook.com>
2022-01-29 19:16:08 +0000

    first commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        /* config for next-optimized-images */
+      },
+    ],
+    // your other plugins here
+  ],
+  {
+    reactStrictMode: true,
+  }
+);
