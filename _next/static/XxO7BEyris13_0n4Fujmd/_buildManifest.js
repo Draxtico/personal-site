4cb980a61ba272572880999f2220c4edb0a6d6c4
RobertoM80 <bronzetto@outlook.com>
2022-01-30 15:49:44 +0000

    Deploying to gh-pages from @ Draxtico/personal-site@85c8ba97b95092cb22684d6b2847fccd303c6748 🚀

---
+++ _next/static/XxO7BEyris13_0n4Fujmd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b6bc9cbf960d5d0c.css","static/chunks/pages/index-9e38780f6f74299a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
